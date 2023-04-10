dbe59e022c093b59e158497bbb1f5687cc5178ea
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2023-04-10 07:24:27 +0530

    add: word doc input parser

---
+++ inputs/docx_parser.py
@@ -0,0 +1,16 @@
+from .parser import Parser
+from docx import Document as docx_document
+import os
+
+class Docx_Parser(Parser):
+    def parse(self):
+        doc = docx_document(self.input_file)
+
+        all_paragraphs = []
+        for para in doc.paragraphs:
+            encoded_para = para.text.encode("ascii", "ignore")
+            decoded_para = encoded_para.decode("ascii", "ignore")
+            all_paragraphs.append(decoded_para)
+
+        extracted_text = os.linesep.join(all_paragraphs)
+        return extracted_text
